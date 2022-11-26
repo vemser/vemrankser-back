8a4073beb10fe513121fd3c57c93c837a847cd29
Alison8547 <alisonailson100@hotmail.com>
2022-11-26 09:02:57 -0300

    config e projeto

---
+++ src/main/java/br/com/vemrankser/ranqueamento/RanqueamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.vemrankser.ranqueamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RanqueamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RanqueamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vemrankser/ranqueamento/RanqueamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vemrankser.ranqueamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RanqueamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
