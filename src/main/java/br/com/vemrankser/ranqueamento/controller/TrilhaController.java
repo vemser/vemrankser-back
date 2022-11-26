ab17af32017dbc27d71b47493dd3f9256a08d595
JONAS DE LARA <jonasdelara_@Hotmail.com>
2022-11-26 16:21:14 -0300

    adicionando documentacao para endpoint trilha

--- src/main/java/br/com/vemrankser/ranqueamento/controller/TrilhaController.java
+++ src/main/java/br/com/vemrankser/ranqueamento/controller/TrilhaController.java
@@ -4,6 +4,9 @@ import br.com.vemrankser.ranqueamento.dto.TrilhaCreateDTO;
 import br.com.vemrankser.ranqueamento.dto.TrilhaDTO;
 import br.com.vemrankser.ranqueamento.exceptions.RegraDeNegocioException;
 import br.com.vemrankser.ranqueamento.service.TrilhaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
@@ -24,7 +27,14 @@ import javax.validation.Valid;
 public class TrilhaController {
 
     private final TrilhaService trilhaService;
-
+    @Operation(summary = "Cadastro de Trilha", description = "Cadastrar Trilha para os módulos")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Cadastro de Trilha realizado com sucesso"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
     @PostMapping
     public ResponseEntity<TrilhaDTO> create(@RequestBody @Valid TrilhaCreateDTO trilhaCreateDTO) throws RegraDeNegocioException {
         log.info("Criando Trilha...");
