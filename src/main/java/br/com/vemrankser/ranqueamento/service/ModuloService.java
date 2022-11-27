d2951f1d61936a8aed15b8ddcd18db8850fc8586
JONAS DE LARA <jonasdelara_@Hotmail.com>
2022-11-27 20:53:59 -0300

    adicionando metodo vincular modulo trilha

--- src/main/java/br/com/vemrankser/ranqueamento/service/ModuloService.java
+++ src/main/java/br/com/vemrankser/ranqueamento/service/ModuloService.java
@@ -3,6 +3,7 @@ package br.com.vemrankser.ranqueamento.service;
 import br.com.vemrankser.ranqueamento.dto.ModuloCreateDTO;
 import br.com.vemrankser.ranqueamento.dto.ModuloDTO;
 import br.com.vemrankser.ranqueamento.entity.ModuloEntity;
+import br.com.vemrankser.ranqueamento.entity.TrilhaEntity;
 import br.com.vemrankser.ranqueamento.enums.StatusModulo;
 import br.com.vemrankser.ranqueamento.exceptions.RegraDeNegocioException;
 import br.com.vemrankser.ranqueamento.repository.ModuloRepository;
@@ -19,6 +20,8 @@ import java.util.List;
 public class ModuloService {
 
     private final ModuloRepository moduloRepository;
+
+    private final TrilhaService trilhaService;
     private final ObjectMapper objectMapper;
 
 
@@ -41,5 +44,22 @@ public class ModuloService {
                 .stream()
                 .map(modulo -> objectMapper.convertValue(modulo, ModuloDTO.class))
                 .toList();
+    }
+
+    public ModuloDTO vincularModuloTrilha(ModuloCreateDTO modulo, Integer id) throws RegraDeNegocioException {
+        ModuloEntity moduloEntity = objectMapper.convertValue(modulo, ModuloEntity.class);
+        moduloEntity.setIdModulo(moduloEntity.getIdModulo());
+        modulo.getTrilhas().forEach(moduloTrilha -> {
+            TrilhaEntity trilha = null;
+            try {
+                trilha = trilhaService.buscarPorIdTrilha(moduloTrilha.getIdTrilha());
+                moduloEntity.getTrilhas().add(trilha);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+        moduloEntity.setStatusModulo(StatusModulo.S);
+        moduloRepository.save(moduloEntity);
+        return objectMapper.convertValue(moduloEntity, ModuloDTO.class);
     }
 }
