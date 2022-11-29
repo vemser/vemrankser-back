d0f80a68b3ca341b14401fba40b36ce2d725c662
JONAS DE LARA <jonasdelara_@Hotmail.com>
2022-11-29 16:07:29 -0300

    merge

--- src/main/java/br/com/vemrankser/ranqueamento/service/TrilhaService.java
+++ src/main/java/br/com/vemrankser/ranqueamento/service/TrilhaService.java
@@ -14,6 +14,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Slf4j
