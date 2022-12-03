f194fd9404902485a2720ddbf22476e6ad966acc
Felipe-Noguez <felipenoguez@outlook.com.br>
2022-12-03 16:02:39 -0300

    Testes em AtividadeServiceTest

--- src/test/java/br/com/vemrankser/ranqueamento/service/AtividadeServiceTest.java
+++ src/test/java/br/com/vemrankser/ranqueamento/service/AtividadeServiceTest.java
@@ -1,9 +1,12 @@
 package br.com.vemrankser.ranqueamento.service;
 
 import br.com.vemrankser.ranqueamento.dto.AtividadeDTO;
+import br.com.vemrankser.ranqueamento.dto.AtividadeNotaDTO;
 import br.com.vemrankser.ranqueamento.dto.ComentarioDTO;
+import br.com.vemrankser.ranqueamento.dto.PageDTO;
 import br.com.vemrankser.ranqueamento.entity.AtividadeEntity;
 import br.com.vemrankser.ranqueamento.entity.ComentarioEntity;
+import br.com.vemrankser.ranqueamento.enums.AtividadeStatus;
 import br.com.vemrankser.ranqueamento.exceptions.RegraDeNegocioException;
 import br.com.vemrankser.ranqueamento.repository.AtividadeRepository;
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -16,6 +19,8 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.List;
@@ -46,22 +51,25 @@ public class AtividadeServiceTest {
         ReflectionTestUtils.setField(atividadeService, "objectMapper", objectMapper);
     }
 
-    @Test
-    public void deveTestarlistarAtividadePorIdTrilhaIdModuloComSucesso() throws RegraDeNegocioException {
-        // Criar variaveis (SETUP)
-        Integer idAtividade = 11;
-        AtividadeEntity atividadeEntity = getAtividadeEntity();
-        atividadeEntity.setIdAtividade(idAtividade);
-        when(atividadeRepository.findById(anyInt())).thenReturn(Optional.of(atividadeEntity));
-
-        // Ação (ACT)
-        AtividadeDTO atividadeDTO = atividadeService.findById(idAtividade);
-
-        // Verificação (ASSERT)
-        assertNotNull(atividadeDTO);
-        assertEquals(11, atividadeDTO.getIdAtividade());
-
-    }
+//    @Test
+//    public void deveTestarlistarAtividadePorIdTrilhaIdModuloComSucesso() throws RegraDeNegocioException {
+//        // Criar variaveis (SETUP)
+//        Integer idTrilha = 1;
+//        Integer idModulo = 2;
+//        Integer pagina = 0;
+//        Integer tamanho = 6;
+//        AtividadeNotaDTO atividadeNotaDTO = getAtividadeNotaDTO();
+//        AtividadeEntity atividadeEntity = getAtividadeEntity();
+////        when(atividadeRepository.listarAtividadePorIdTrilhaIdModulo(any(), anyInt(), anyInt(), AtividadeStatus.CONCLUIDA)).thenReturn((Page<AtividadeNotaDTO>) atividadeEntity);
+//
+//        // Ação (ACT)
+//        PageDTO pageDTO = atividadeService.listarAtividadePorIdTrilhaIdModulo(pagina, tamanho, idTrilha, idModulo, AtividadeStatus.CONCLUIDA);
+//
+//        // Verificação (ASSERT)
+//        assertNotNull(pageDTO);
+////        assertEquals(11, pageDTO.getIdAtividade());
+//
+//    }
 
     @Test
     public void deveTestarbuscarPorIdAtividadeComSucesso() throws RegraDeNegocioException {
@@ -121,5 +129,15 @@ public class AtividadeServiceTest {
         atividadeEntity.setNomeInstrutor("Robert Plant");
 
         return atividadeEntity;
+    }
+
+    public static AtividadeNotaDTO getAtividadeNotaDTO () {
+        AtividadeNotaDTO atividadeNotaDTO = new AtividadeNotaDTO();
+
+        atividadeNotaDTO.setIdAtividade(11);
+        atividadeNotaDTO.setNota(100);
+        atividadeNotaDTO.setNome("Java OO");
+
+        return atividadeNotaDTO;
     }
 }
